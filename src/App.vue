7694f8442cefa56bef0ef58a21b8d96f7317939b
Hector Romero <hectorromerodev@gmail.com>
2022-11-09 21:28:07 -0800

    update flex app

--- src/App.vue
+++ src/App.vue
@@ -39,8 +39,10 @@ header {
 @media (min-width: 1024px) {
   header {
     display: flex;
+    flex-direction: column;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
+    gap: 2rem;
   }
 
   .logo {
